17aef72d3cf7c454169fc7b41f84a96c0b13b587
designwithpinsara <designwithpinsarasasika@gmail.com>
2026-06-18 12:33:41 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -73,7 +73,9 @@ function saveGalleryData(data) {
 ========================================= */
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
